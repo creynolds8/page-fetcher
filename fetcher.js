5082c6b33bb4308ccea1d3ce30717855025c17fc
creynolds8 <cameron8.cr@gmail.com>
2024-05-08 20:14:16 -0700

    add get functionality

--- fetcher.js
+++ fetcher.js
@@ -9,3 +9,8 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 
+needle.get(args[0], (error, response, body) => {
+  console.log('error: ', error);
+  console.log('response: ', response && response.statusCode);
+  //something with body, not log
+});
