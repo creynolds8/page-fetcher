729ccd1e8c7b3e6a9004b37ca86bcdac4f4fe52c
creynolds8 <cameron8.cr@gmail.com>
2024-05-08 20:26:28 -0700

    add write functionality,

--- fetcher.js
+++ fetcher.js
@@ -9,8 +9,14 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 
+const write = function(body) {
+  fs.writeFile(args[1], body, err => {
+    if (err) console.log('error ', err);
+    else console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
+  });
+};
 needle.get(args[0], (error, response, body) => {
   console.log('error: ', error);
   console.log('response: ', response && response.statusCode);
-  //something with body, not log
+  write(body);
 });
